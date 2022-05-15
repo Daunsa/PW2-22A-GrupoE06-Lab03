a2db2972cbb597e2ba7207c5071a9e0187a71377
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-15 14:18:45 -0500

    Se creo el metodo front y back end para enviar los datos

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.listen(3000, () => {
+    console.log("Escuchando en: http://localhost:3000")
+
+});
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, './public/index.html'));
+});
+
+app.post('/nuevo', (request, response) => {
+    response.setHeader('Content-type', 'text/plain');
+    const nombre = request.body.nombre;
+    const texto = request.body.texto;
+
+    var fs = require('fs');
+
+    fs.appendFile(nombre + '.txt', texto, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    response.setHeader('Content-type', 'html/plain');
+    response.send('Datos guardados con éxito');
+});
