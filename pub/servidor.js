7b94db423511418daf96d3ee0c36e3217ce30097
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-16 19:55:29 -0500

    Completando funcion para listar

--- pub/servidor.js
+++ pub/servidor.js
@@ -6,3 +6,15 @@ function listar() {
         renderList(data);
       });
   }
+  function renderList(data) {
+    let html = "<h3>Archivos</h3><hr>\n";
+    html += "<ul>\n";
+    const contenedor = document.querySelector("#boxContent-second");
+  
+    for (var i = 0; i < data.length; i++){
+      html += "<li class='file' onclick='leer(this)'>" + data[i] + "</li>\n";
+    } 
+    
+    html += "</ul>\n";
+    contenedor.innerHTML = html;
+  }
